6303ad423c1ddde7faf893cc3057c40f5326eac1
Biagio Festa <biagio.festa@gmail.com>
2016-06-07 12:01:29 +0200

    Added guards and lic on file Algorithm.hpp

--- src/Algorithm.hpp
+++ src/Algorithm.hpp
@@ -1,4 +1,25 @@
 // Copyright 2016 <Biagio Festa>
+
+/*
+  This file is part of PAP-BCO_Solver.
+
+  PAP-BCO_Solver is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+    
+  PAP-BCO_Solver is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+    
+  You should have received a copy of the GNU General Public License
+  along with PAP-BCO_Solver.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __PAP_BCO_PARSER__ALGORITHM__HPP
+#define __PAP_BCO_PARSER__ALGORITHM__HPP
+
 #include <map>
 #include <algorithm>
 #include <queue>
@@ -440,3 +461,6 @@ void Algorithm<Graph, RndGenerator>::assign_accordance_spanning_tree(
 }
 
 }  // namespace pap_solver
+
+
+#endif  // __PAP_BCO_PARSER__ALGORITHM__HPP
