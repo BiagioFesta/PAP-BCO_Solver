14ca0307518e27ca7238e7be5fdb637d9c0a9714
Biagio Festa <biagio.festa@gmail.com>
2016-06-15 16:50:50 +0200

    Erased useless files

--- validation/optimal_model/eval.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./create_datafile_from_matrix.py "$@" > data.dat && \
-echo OPTIMAL SOLUTION && \
-(time ampl ampl.run) 2>&1 | grep -P '(?:objective|real)' && \
-echo PAP-SOLVER && \
-(time ../../build/pap-bco_solver $1) 2>&1 | grep -P '(?:PortAB|real|edges)' && \
-rm data.dat
